919ec5fc6a68cf0e3f2150a9cd38ef5de2905c84
Ponmander <ponmander@gmail.com>
2025-02-14 21:38:56 -0600

    First commit

---
+++ src/world/area_nok/nok_11/generated.h
@@ -0,0 +1,14 @@
+/* auto-generated, do not edit */
+#include "star_rod_macros.h"
+
+// type: MapProperties
+#define GEN_MAP_LOCATION LOCATION_GOOMBA_ROAD
+
+// type: Marker:CamTarget
+// name: CutsceneCam
+#define GEN_CUTSCENE_CAM_X -199
+#define GEN_CUTSCENE_CAM_Y 0
+#define GEN_CUTSCENE_CAM_Z -48
+#define GEN_CUTSCENE_CAM_DIR 0
+#define GEN_CUTSCENE_CAM_VEC -199,0,-48
+
--- src/world/area_nok/nok_11/nok_11.h
+++ src/world/area_nok/nok_11/nok_11.h
@@ -2,6 +2,7 @@
 /// @brief Koopa Region - Pleasant Path Entry
 
 #include "common.h"
+#include "generated.h"
 #include "message_ids.h"
 #include "map.h"
 
